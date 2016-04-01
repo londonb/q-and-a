af8e4ee574522b6ce16180ce626cbe9318f3811f
Brad London <london.brad@gmail.com>
2016-04-01 13:28:13 -0700

    return list of questions to the index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return Ember.RSVP.hash({
-      answers: this.store.findAll('answer'),
-      questions: this.store.findAll('question')
-    });
-  }
+  model(){
+    return this.store.findAll('question');
+  },
 });
