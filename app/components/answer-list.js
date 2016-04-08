bba536f8719fdfd905e0a31cafb3e75c7e3c78ac
Brad London <london.brad@gmail.com>
2016-04-08 14:05:14 -0700

    remove some unused code

--- app/components/answer-list.js
+++ app/components/answer-list.js
@@ -4,12 +4,4 @@ export default Ember.Component.extend({
   sortBy: ['rating:desc'],
   sortedRankings: Ember.computed.sort('question.answers', 'sortBy'),
 
-  questionRating: Ember.computed('sortedRankings', function() {
-    var total = 0;
-    for (var i = 0; i < this.get('sortedRankings.length'); i++) {
-      total += this.get('sortedRankings')[i].get('rating');
-    }
-    return total;
-  })
-
 });
