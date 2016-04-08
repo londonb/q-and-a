2ffc47a45089793320ba5ce2f0895deb64b78a12
Brad London <london.brad@gmail.com>
2016-04-08 12:18:44 -0700

    sort comments by rank

---
+++ app/components/answer-list.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  sortBy: ['rating:desc'],
+  sortedRankings: Ember.computed.sort('question.answers', 'sortBy')
+});
--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // questionRating: Ember.computed('answer', function() {
+  //   var total = 0;
+  //   for (var i = 0; i < this.get('answer.length'); i++) {
+  //     total += this.get('answer')[i].get('rating');
+  //   }
+  //   console.log (total);
+  //   return total;
+  // })
+
 });
